4cf8cefaeb0848a94b3e3f08e815196e21624b1d
Daos13 <100082446+Daos13@users.noreply.github.com>
2022-02-20 20:49:39 +0300

    Add files via upload

---
+++ declination.py
@@ -0,0 +1,9 @@
+numbers = list(range(1, 101))
+
+print(numbers[0], ('процент'))
+
+for number in numbers[1:5]:
+    print(number, 'процентa')
+
+for number in numbers[5::]:
+    print(number, 'процентов')
